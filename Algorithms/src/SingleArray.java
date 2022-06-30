8de018ef3d795ce340f1e6169e6a5e9d4979cc09
EvansonBarasa <evanzo85@gmail.com>
2022-06-30 12:41:46 +0300

    DS commit

---
+++ Algorithms/src/SingleArray.java
@@ -0,0 +1,20 @@
+
+public class SingleArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[]nums;
+		nums = new int[4];
+		
+		nums[0] = 1;
+		nums[1] = 2;
+		nums[2] = 3;
+		nums[3] = 4;
+		//int[]nums = {1,2,3,4}
+		
+		System.out.println(nums[3]);
+
+	}
+
+}
